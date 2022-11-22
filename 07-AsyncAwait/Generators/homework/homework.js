86af5211ae22b76d996cb05412fd1ca9cb201e0c
LucianoRp1 <105827151+LucianoRp1@users.noreply.github.com>
2022-11-22 20:40:23 -0300

    Primer comienzo

---
+++ 07-AsyncAwait/Generators/homework/homework.js
@@ -0,0 +1,17 @@
+function fizzBuzzGenerator(max) {
+  // Tu código acá:
+  let i = 1;
+  while(max ? i <= max : true){
+    if( i % 3 === 0 && i % 5 === 0){
+      yield 'Fizz Buzz';
+    } else if (i % 3 === 0){
+      yield 'Fizz';
+    } else if (i % 5 === 0){
+      yield 'Buzz';
+    }
+    i++;
+  }
+
+}
+
+module.exports = fizzBuzzGenerator;
